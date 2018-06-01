ded5b43dfb826acbd9cc4d5ae3abd07a7bb41673
Abhinav Mittra <abhinavmittra@yahoo.com>
2018-06-01 19:58:58 +0530

    Updated model

---
+++ extract.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+from glob import glob
+from keras.preprocessing.image import *
+from behind_the_scenes.model import CNN_model
+
+WA_path = input('Enter absolute path of WhatsApp folder: \n')
+
+WA_img_path = WA_path + '/Media/WhatsApp Images/'
+WA_img_path.replace('//', '/') 
+WA_img_path.replace(' ', '\\ ') #replace spaces with their escaped versions
+
+# define model
+model = CNN_model()
+# load trained weights
+model.load_weights('details/weights.h5')
+
+notes_path = WA_img_path + 'notes/'
+if not os.path.exists(notes_path):
+    os.mkdir(notes_path)  #Create a new folder called notes to save all the notes.
+
+def predict(file_path):
+    
+    #predict whether file is a notes image
+    
+    img = load_img(file_path, target_size=(124, 124, 3))
+    x = img_to_array(img) / 255. 
+    y = model.predict(np.expand_dims(x, axis=0))
+    return np.squeeze(y) > 0.5
+
+
+# get file paths 
+files = glob(WA_img_path + '*.*') + glob(WA_img_path + 'Sent/*.*')
+
+# extract notes from WhatsApp Images folder
+
+for count, file_path in enumerate(files):
+    if not count % 10: print(str(count) + ' files examined')
+    if predict(file_path): # check if the file is one of the notes
+        file_name = file_path.split('/')[-1] # get file name
+        os.rename(file_path, notes_path + file_name) # move the file to 'notes' folder
+
